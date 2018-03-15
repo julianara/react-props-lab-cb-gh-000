e6035214a652dc3595a03d5fa65ba44406a7482e
Julia Nara <julianara888@gmail.com>
2018-03-15 13:00:00 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -7,7 +7,14 @@ export default class Spaceship extends React.Component {
         <h1>{this.props.name}</h1>
         <p>Speed: {this.props.speed}</p>
         <p>Has Rockets: {this.props.hasRockets}</p>
+<<<<<<< HEAD
         <p>Colors: {this.props.colors.join(', ')} </p>
+=======
+        <p>Colors: </p>
+        
+        <p></p>
+        <p></p>
+>>>>>>> 1b94445a052536e7443ed5e97418fe9a2e9f56aa
       </div>
     );
   }
